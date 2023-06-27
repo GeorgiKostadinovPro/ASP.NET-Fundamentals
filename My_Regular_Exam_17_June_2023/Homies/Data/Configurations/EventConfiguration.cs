5aaf9d865822073298b81882d7ce8a5399f32eb6
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2023-06-27 19:53:16 +0300

    all functionality implemented

---
+++ My_Regular_Exam_17_June_2023/Homies/Data/Configurations/EventConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Homies.Data.Common.Constants;
+using Homies.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Homies.Data.Configurations
+{
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(ValidationalConstants.EventConstants.EventNameMaxLength);
+
+            builder.Property(e => e.Description)
+                .IsRequired()
+                .HasMaxLength(ValidationalConstants.EventConstants.EventDescriptionMaxLength);
+        }
+    }
+}
