fb36888f206995ea0534743ddc34d3b322bf4f28
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2023-06-27 19:47:22 +0300

    all functionality implemented

---
+++ My_Regular_Exam_17_June_2023/Homies/Data/Configurations/TypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Homies.Data.Common.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Homies.Data.Configurations
+{
+    public class TypeConfiguration : IEntityTypeConfiguration<Models.Type>
+    {
+        public void Configure(EntityTypeBuilder<Models.Type> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(ValidationalConstants.TypeConstants.TypeNameMaxLength);
+        }
+    }
+}
