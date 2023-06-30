914340bf36f9a872ab4e5a78e00e93e923571280
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2023-06-30 12:41:46 +0300

    project structure implemented

---
+++ My_Regular_Exam_17_June_2023/Homies/Program.cs
@@ -0,0 +1,68 @@
+using Homies.Data;
+using Homies.Data.Common.Repositories;
+using Homies.Services;
+using Homies.Services.Contracts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<HomiesDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<HomiesDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+});
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddMvc();
+
+// Auto mapper
+builder.Services.AddAutoMapper(typeof(Program));
+
+// Repository pattern
+builder.Services.AddScoped<IRepository, Repository>();
+
+// Services
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<ITypeService, TypeService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
